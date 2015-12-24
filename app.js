28882fdd9f5d704d7ef635a1ad2c668f2fac3b9e
benstew <benstewart2008@gmail.com>
2015-12-24 13:54:23 -0500

    Added images, refactored some logic away from the view

--- app.js
+++ app.js
@@ -5,20 +5,44 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+
+  });
+
   var gems = [
     {
      name: 'Dodecahedron',
-     price: 2.95,
+     price: 2,
      description: 'Nice and pretty',
      canPurchase: true,
-     soldOut: false
+     soldOut: false,
+     images: [
+       {
+         full:'dodeca-gem.jpg',
+         thumb:''
+       },
+     ]
    },
    {
     name: 'Pentagonal Gem',
-    price: 5.95,
+    price: 5.9,
     description: 'Oh dang, did you see that rock!',
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: [
+      {
+        full:'',
+        thumb:''
+      },
+    ]
   }
   ];
 
